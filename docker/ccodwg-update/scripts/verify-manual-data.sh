389155d35ed09bb17e33b34b6738536c90c7117e
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2021-12-29 19:04:14 -0500

    Add workflow for verifying manual data have been inputed today.

---
+++ docker/ccodwg-update/scripts/verify-manual-data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# clone scripts from Covid19Canada
+git clone \
+  --depth 1  \
+  --filter=blob:none  \
+  --sparse \
+  https://github.com/ccodwg/Covid19Canada
+cd Covid19Canada
+git sparse-checkout set scripts
+
+# export GitHub PAT
+export GITHUB_PAT=$GH_PAT
+
+# run verify_data_sources.R
+Rscript "scripts/verify_manual_data.R"
