e4572fe4664eecd20ee587caff83ad91334d9669
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2021-12-30 13:35:00 -0500

    Update ccodwg-archive workflows.

--- docker/ccodwg-archive/scripts/ccodwg-archive-prod.sh
+++ docker/ccodwg-archive/scripts/ccodwg-archive-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -a; source archive.env; set +a
 cd Covid19CanadaArchive
-/usr/local/bin/python archiver.py -m prod
+/usr/local/bin/python archiver.py datasets.json -m prod
--- docker/ccodwg-archive/scripts/ccodwg-archive-test.sh
+++ docker/ccodwg-archive/scripts/ccodwg-archive-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -a; source archive.env; set +a
 cd Covid19CanadaArchive
-/usr/local/bin/python archiver.py -m test
+/usr/local/bin/python archiver.py datasets.json -m test
