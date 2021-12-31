af5be095995f056053192f1dcf9f768c651d1dcf
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2021-12-30 19:45:50 -0500

    Update ccodwg-archive-test with debug parameters.

--- docker/ccodwg-archive/scripts/ccodwg-archive-test.sh
+++ docker/ccodwg-archive/scripts/ccodwg-archive-test.sh
@@ -8,5 +8,6 @@ cd Covid19CanadaArchive
                                   8d0aa5a5-6397-4f4c-a9ca-26ba81f26bc7 \
                                   6edc20de-c174-4c75-9d6c-c463ef43512b \
                                   7ae6a613-aa31-4f63-aaeb-f8fdf3d68ee0 \
-                                  45b626ef-d41d-48e9-b007-97edac5ac838
+                                  45b626ef-d41d-48e9-b007-97edac5ac838 \
+                                  -d print-md5
 
