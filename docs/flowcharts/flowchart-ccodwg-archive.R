b38bc6f9e52a5a31c13b6e38f1dccc2864d1b3b2
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2022-06-25 16:53:29 -0400

    Update ccodwg-archive flowchart
    - Update commands in ccodwg-archive flowchart
    - Add index step to ccodwg-archive flowchart
    - Closes #27

--- docs/flowcharts/flowchart-ccodwg-archive.R
+++ docs/flowcharts/flowchart-ccodwg-archive.R
@@ -16,20 +16,21 @@ flowchart_ccodwg_archive <- grViz('digraph flowchart {
       datasets_no_js [color = grey, label = "Datasets not requiring JS\n(e.g., CSV, JSON, simple HTML)"]
       datasets_js [color = grey, label = "Datasets requiring JS\n(complex HTML)"]
       archivist [color = grey, label = <<i>archivist</i>>]
-      archivist_test [color = blue, label = <Periodic automated testing<br/>of dataset downloads<br/><i>(archivist -m test)</i>>]
-      archivist_prod [color = blue, label = <Nightly archive update<br/><i>(archivist -m prod)</i>>]
+      archivist_test [color = blue, label = <Periodic automated testing<br/>of dataset downloads<br/><i>(python -m archivist test)</i>>]
+      archivist_prod [color = blue, label = <Nightly archive update<br/><i>(python -m archivist prod)</i>>]
       test_report [color = blue, label = "Email report of datasets that\ncould not be downloaded (if any)"]
       prod_report [color = blue, label = "Email report on datasets that\ncould not be archived (if any)"]
       manual_update_datasets [color = red, label = <Manually update list of<br/>datasets, if necessary<br/><i>(datasets.json)</i>>]
-      manual_rerun [color = red, label = <Manually archive failed<br/>datasets, if necessary<br/><i>(archivist -m prod --uuid)</i>>]
+      manual_rerun [color = red, label = <Manually archive failed<br/>datasets, if necessary<br/><i>(python -m archivist prod --uuid)</i>>]
       upload_s3 [color = blue, label = <Upload datasets<br/>to public S3 bucket<br/><i>(data.opencovid.ca)</i>>]
+      regen_index [color = blue, label = <Regenerate index of available files<br/><i>python -m archivist index</i>>]
       
       # edge definitions with the node IDs
       datasets_no_js:s -> archivist [label = <download w/ <i>requests</i>>]
       datasets_js:s -> archivist [label = <download w/ <i>selenium</i>>]
       archivist -> archivist_test -> test_report -> manual_update_datasets
       archivist -> archivist_prod -> prod_report -> manual_rerun
-      archivist_prod -> upload_s3
+      archivist_prod -> upload_s3 -> regen_index
       
       }')
 
