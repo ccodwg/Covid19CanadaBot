b4069abf99ee9f594fd8a52f8b6185b96f599faa
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2022-11-26 18:30:38 -0500

    Use GitHub Actions bot as the commit author for ccodwg-update
    - Use user.name and user.email suggested in this GitHub discussion: https://github.com/actions/checkout/discussions/479
    - Closes #31

--- docker/ccodwg-update/scripts/update-plots.sh
+++ docker/ccodwg-update/scripts/update-plots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git config --global user.name 'Jean-Paul R. Soucy'
-git config --global user.email 'jeanpaul.r.soucy@gmail.com'
+git config --global user.name 'github-actions[bot]'
+git config --global user.email 'github-actions[bot]@users.noreply.github.com'
 git clone --depth=1 https://jeanpaulrsoucy:$GH_PAT@github.com/ccodwg/CovidTimelineCanadaPlots.git /CovidTimelineCanadaPlots
 cd /CovidTimelineCanadaPlots
 export GITHUB_PAT=$GH_PAT
