967df001f6b5079502b32caf6eed6664aa9e97ae
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2022-12-13 23:12:46 -0500

    Remove update-plots.sh
    - Retire CovidTimelineCanadaPlots repository in favour of an automatically updated JavaScript dashboard (see ccodwg/CovidTimelineCanada#93 and ccodwg/CovidTimelineCanada#12)

--- docker/ccodwg-update/scripts/update-plots.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git config --global user.name 'github-actions[bot]'
-git config --global user.email 'github-actions[bot]@users.noreply.github.com'
-git clone --depth=1 https://jeanpaulrsoucy:$GH_PAT@github.com/ccodwg/CovidTimelineCanadaPlots.git /CovidTimelineCanadaPlots
-cd /CovidTimelineCanadaPlots
-export GITHUB_PAT=$GH_PAT
-Rscript "update_plots.R"
