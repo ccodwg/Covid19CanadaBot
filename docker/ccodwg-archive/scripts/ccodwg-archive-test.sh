6fd6a2021d604710c8621adc686f52995f493a72
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2023-01-11 23:36:46 -0500

    Run rerun code for failed datasets (#35)

--- docker/ccodwg-archive/scripts/ccodwg-archive-test.sh
+++ docker/ccodwg-archive/scripts/ccodwg-archive-test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# create temporary file
+tmpfile=$(mktemp /tmp/archive-run.XXXXXX)
+exec 3>"$tmpfile"
+exec 4<"$tmpfile"
+rm "$tmpfile"
+# run script
 set -a; source archive.env; set +a
 cd /Covid19CanadaArchive
 /usr/local/bin/python -m archivist \
@@ -12,4 +18,21 @@ cd /Covid19CanadaArchive
                                   45b626ef-d41d-48e9-b007-97edac5ac838 \
                                   3ed8e857-f5d5-4315-b083-b316e3bb18fd \
                                   80c53b1a-1e74-4871-9d9c-920f40e663e9 \
-                                --debug print-md5
+                                  --debug print-md5 \
+                                  2>&3
+# read stderr and run rerun code, if necessary
+RERUN="$(head -n 1 <&4)"
+if [ -z "$RERUN" ]
+then
+	echo "Script completed without issue."
+else
+  	echo "Script for failed datasets will run after 10 minutes..."
+	# wait 10 minutes
+	sleep 10m
+	# run script
+  	echo "Rerunning script for failed datasets..."
+	eval $RERUN
+	echo "Script completed."
+fi
+# close file descriptor
+exec 3>&-
